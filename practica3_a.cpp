266985c9806166b06fd9ee40fa54d405c35f5097
benchimol2305 <cesarbenchimoldavila@gamil.com>
2024-11-18 19:31:09 -0400

    se realiza una funcion para determinar el cuadrante de una coordenada

--- practica3_a.cpp
+++ practica3_a.cpp
@@ -6,3 +6,21 @@ struct Coordenada {
     int x;
     int y;
 };
+
+string determinarCuadrante(const Coordenada& coord) {
+    if (coord.x > 0 && coord.y > 0) {
+        return "Primer cuadrante";
+    } else if (coord.x < 0 && coord.y > 0) {
+        return "Segundo cuadrante";
+    } else if (coord.x < 0 && coord.y < 0) {
+        return "Tercer cuadrante";
+    } else if (coord.x > 0 && coord.y < 0) {
+        return "Cuarto cuadrante";
+    } else if (coord.x == 0 && coord.y != 0) {
+        return "Eje Y";
+    } else if (coord.x != 0 && coord.y == 0) {
+        return "Eje X";
+    } else {
+        return "Origen";
+    }
+}
