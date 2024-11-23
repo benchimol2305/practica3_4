60af1247818cf322199d28086fdbcf6624034d96
benchimol2305 <cesarbenchimoldavila@gamil.com>
2024-11-23 18:03:07 -0400

    se calcula el vector direccion de la primera linea

---
+++ practica4_b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Punto {
+    double x, y;
+};
+
+double calcularAngulo(const Punto& inicio1, const Punto& fin1, const Punto& inicio2, const Punto& fin2, bool enGrados = false) {
+    double dx1 = fin1.x - inicio1.x;
+    double dy1 = fin1.y - inicio1.y;
