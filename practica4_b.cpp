60cba659ba231005ae477a8d358b78680b7d3590
benchimol2305 <cesarbenchimoldavila@gamil.com>
2024-11-23 18:05:08 -0400

    se realiza el producto escalar de los vectores

--- practica4_b.cpp
+++ practica4_b.cpp
@@ -9,6 +9,8 @@ struct Punto {
 double calcularAngulo(const Punto& inicio1, const Punto& fin1, const Punto& inicio2, const Punto& fin2, bool enGrados = false) {
     double dx1 = fin1.x - inicio1.x;
     double dy1 = fin1.y - inicio1.y;
-    
+
     double dx2 = fin2.x - inicio2.x;
     double dy2 = fin2.y - inicio2.y;
+
+    double productoEscalar = dx1 * dx2 + dy1 * dy2;
